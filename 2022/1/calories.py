1136cf8d4618eb27a97530e1f8dc2db9f98505b7
eina <109956262+encarbassotnopot@users.noreply.github.com>
2022-12-01 17:35:36 +0100

    dia 1 a veure fins on arribem xd

---
+++ 2022/1/calories.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+calories = []
+
+with open("input.txt") as f:
+	lines = f.readlines()
+	sum = 0
+
+	for line in lines:
+		line = line.strip()
+		if line == '':
+			calories.append(sum)
+			sum = 0
+		else:
+			sum += int(line)
+		
+	calories.append(sum)
+
+top3 = [0, 0, 0]
+
+for cal in calories:
+	if cal > calories[2]:
+		top3.append(cal)
+		top3.sort(reverse=True)
+		top3.pop()
+
+print(top3[0])
+print(top3[0] + top3[1] + top3[2])
